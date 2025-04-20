6157fcd9130538e509ea783747119b26be67e6db
capybara97 <bezdomnyi1997@gmail.com>
2025-04-20 11:23:36 +0500

    hw2

---
+++ src/main/java/org/skypro/skyshop/service/BasketService.java
@@ -0,0 +1,32 @@
+package org.skypro.skyshop.service;
+
+import org.skypro.skyshop.model.basket.BasketItem;
+import org.skypro.skyshop.model.basket.ProductBasket;
+import org.skypro.skyshop.model.basket.UserBasket;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BasketService {
+    private final StorageService storageService;
+    private final ProductBasket productBasket;
+
+    public BasketService(StorageService storageService, ProductBasket productBasket) {
+        this.storageService = storageService;
+        this.productBasket = productBasket;
+    }
+
+    public void addProductInBasket(UUID id){
+        if(storageService.getProductById(id).isEmpty()) throw new IllegalArgumentException("Товар не найден");
+        productBasket.addProductInBasket(id);
+    }
+
+    public UserBasket getUserBasket(){
+        List<BasketItem> list = productBasket.getBasket().entrySet().stream()
+                .map(el -> new BasketItem(storageService.getProductById(el.getKey()).get(), el.getValue()))
+                .toList();
+        return new UserBasket(list);
+    }
+}
